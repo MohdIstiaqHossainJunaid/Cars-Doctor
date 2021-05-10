af5cf81aed63ac401509881103588e3be1da9626
Shazzadul Islam Fahim 1811195042 <shazzadul.fahim@northsouth.edu>
2021-05-10 16:51:14 +0600

    Complete

--- register/car_doctor/car_doctor/settings.py
+++ register/car_doctor/car_doctor/settings.py
@@ -127,9 +127,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
 STATIC_URL = '/templates/'
 
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
     os.path.join(BASE_DIR, 'templates/'),
 ]
 MEDIA_URL = '/image/'
--- register/car_doctor/products/views.py
+++ register/car_doctor/products/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from . models import productlist
 
 
 # Create your views here.
 
 
 def productlist(request):
-    pl = productlist.objects.all()
-    return render(request, 'product.html', {'pl': pl})
+    return render(request, 'product.html')
